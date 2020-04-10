33a62380d64b347c55d66ef9ae5803090052bef0
郝宇帆 <1079422762@qq.com>
2020-04-10 10:14:11 +0800

    Create configureStore.js

---
+++ configureStore.js
@@ -0,0 +1,91 @@
+/**
+ * Create the store with dynamic reducers
+ */
+
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
+import { routerMiddleware } from 'react-router-redux';
+import { LOCATION_CHANGE } from 'react-router-redux';
+
+/*
+ * routeReducer
+ *
+ * The reducer merges route location changes into our immutable state.
+ * The change is necessitated by moving to react-router-redux@5
+ *
+ */
+
+// Initial routing state
+const routeInitialState = {
+  location: null,
+};
+
+/**
+ * Merge route into the global application state
+ */
+function routeReducer(state = routeInitialState, action) {
+  switch (action.type) {
+    /* istanbul ignore next */
+    case LOCATION_CHANGE:
+      return {
+        ...state,
+        location: action.payload,
+      };
+    default:
+      return state;
+  }
+}
+/**
+ * Creates the main reducer with the dynamically injected ones
+ */
+export default function createReducer(injectedReducers) {
+  return combineReducers({
+    route: routeReducer,
+    ...injectedReducers,
+  });
+}
+
+export default function configureStore(initialState = {}, history) {
+  // Create the store with two middlewares
+  // routerMiddleware: Syncs the location/URL path to the state
+  const middlewares = [
+    routerMiddleware(history),
+  ];
+
+  const enhancers = [
+    applyMiddleware(...middlewares),
+  ];
+
+  // If Redux DevTools Extension is installed use it, otherwise use Redux compose
+  /* eslint-disable no-underscore-dangle */
+  const composeEnhancers =
+    process.env.NODE_ENV !== 'production' &&
+    typeof window === 'object' &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+      ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+        // TODO Try to remove when `react-router-redux` is out of beta, LOCATION_CHANGE should not be fired more than once after hot reloading
+        // Prevent recomputing reducers for `replaceReducer`
+        shouldHotReload: false,
+      })
+      : compose;
+  /* eslint-enable */
+
+  const store = createStore(
+    createReducer(),
+    initialState,
+    composeEnhancers(...enhancers)
+  );
+
+  // Extensions
+  store.injectedReducers = {}; // Reducer registry
+
+  // Make reducers hot reloadable, see http://mxs.is/googmo
+  /* istanbul ignore next */
+  // if (module.hot) {
+   //  module.hot.accept('./reducers', () => {
+   //    store.replaceReducer(createReducer(store.injectedReducers));
+   //    store.dispatch({ type: '@@REDUCER_INJECTED' });
+   //  });
+ //  }
+
+  return store;
+}
